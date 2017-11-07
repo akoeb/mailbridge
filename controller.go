02a32f1748b9da12ee0933b6bd435ddec4b4de51
Alexander Köb <projekt@koeb.me>
2017-11-07 16:43:36 +0100

    moving REST request and response objects into controller

--- controller.go
+++ controller.go
@@ -10,22 +10,27 @@ import (
 	"net/http"
 )
 
-type controller struct {
+// Controller is a object that holds all our handlers and their dependencies
+type Controller struct {
 	activeTokens *ActiveTokens
 	mailServer   *MailServer
 	tarpit       *Tarpit
+	bodyLimit    int64
 }
 
-func initController(m *MailServer, a *ActiveTokens, t *Tarpit) *controller {
-	c := &controller{
+// InitController is the factory method for the controller
+func InitController(m *MailServer, a *ActiveTokens, t *Tarpit) *Controller {
+	c := &Controller{
 		activeTokens: a,
 		mailServer:   m,
 		tarpit:       t,
+		bodyLimit:    1048576,
 	}
 	return c
 }
 
-func (c *controller) GetToken(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+// GetToken is the handler for the /token endpoint
+func (c *Controller) GetToken(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	// tarpit the client if we had an earlier request:
 	err := c.tarpit.Wait(r)
@@ -51,8 +56,10 @@ func (c *controller) GetToken(w http.ResponseWriter, r *http.Request, _ httprout
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "%s\n", response)
 }
-func (c *controller) SendMail(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+
+// SendMail is the Handler for the /send endpoint
+func (c *Controller) SendMail(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, c.bodyLimit))
 	if err != nil {
 		log.Printf("ERROR ReadBodyStream: %v", err)
 		http.Error(w, "ERROR", http.StatusBadRequest)
@@ -88,4 +95,64 @@ func (c *controller) SendMail(w http.ResponseWriter, r *http.Request, _ httprout
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+// Request and Response Objects
+
+// TokenResponse represents the response object returned by the token endpoint
+type TokenResponse struct {
+	Token   string
+	Expires int64
+}
+
+// ResponseObjectFromToken is a mapper method that returns a TokenResponse for the HTTP endpoint from a given Token
+func ResponseObjectFromToken(token *Token) *TokenResponse {
+	return &TokenResponse{
+		Token:   token.String(),
+		Expires: token.Expires.Unix(),
+	}
+}
+
+// SendMailRequest represents the accepted structure that clients send to the send endpoint
+type SendMailRequest struct {
+	Token   string `json:"Token"`
+	From    string `json:"From"`
+	To      string `json:"To"`
+	Subject string `json:"Subject"`
+	Body    string `json:"Body"`
+}
+
+// Validate checks whether all required fields are set
+func (in *SendMailRequest) Validate() error {
+	var msg []string
+
+	if in.Token == "" {
+		msg = append(msg, "Token must be set")
+	}
+	if in.From == "" {
+		msg = append(msg, "From must be set")
+	}
+	if in.To == "" {
+		msg = append(msg, "To must be set")
+	}
+	if in.Subject == "" {
+		msg = append(msg, "Subject must be set")
+	}
+	if in.Body == "" {
+		msg = append(msg, "Message Body must be set")
+	}
+	if len(msg) > 0 {
+		return errors.New(strings.Join(msg, "\n"))
+	}
+	return nil
+}
+
+// MessageObjectFromRequest is a mapper method that returns a email message object from a given SendMailRequest
+func MessageObjectFromRequest(request SendMailRequest) *EmailMessage {
+	return &EmailMessage{
+		from:        request.From,
+		recipientID: request.To,
+		subject:     request.Subject,
+		body:        request.Body,
+	}
 }
